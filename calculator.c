84d9790d9455525b487276182db840d022829e02
Lukas <lukas.cothran@siu.edu>
2026-09-03 10:02:14 -0500

    Implmented multiplication feature

--- calculator.c
+++ calculator.c
@@ -2,10 +2,12 @@
 
 int add(int a, int b);
 float sub(float a, float b);
+float mult(float a, float b);
 
 int main(){
     add(5,10);
     sub(10,5);
+    mult(13, 7);
     return 0;
 }
 
@@ -15,4 +17,8 @@ int add(int a, int b){
 
 float sub(float a, float b){
     return a-b;
+}
+
+float mult(float a, float b){
+    return a * b;
 }
