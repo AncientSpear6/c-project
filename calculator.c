a651d0764eaa3ab55a7380a8a1024f2d3754dce2
Lukas <lukas.cothran@siu.edu>
2026-09-03 10:00:19 -0500

    Implemeted subtraction feature

--- calculator.c
+++ calculator.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
 int add(int a, int b);
+float sub(float a, float b);
 
 int main(){
     add(5,10);
+    sub(10,5);
     return 0;
 }
 
 int add(int a, int b){
     return a + b;
+}
+
+float sub(float a, float b){
+    return a-b;
 }
