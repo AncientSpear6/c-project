a949cb6325df5a5b243cb8353b06bb13a84f6f5d
Lukas <lukas.cothran@siu.edu>
2026-09-03 10:54:03 -0500

    Added comments and documentation

--- calculator.c
+++ calculator.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-float add(float a, float b);
+float add(float a, float b);//prototypes of functions
 float sub(float a, float b);
 float mult(float a, float b);
 float div(float a, float b);
@@ -9,13 +9,13 @@ int main(){
     int operation;
     float ans;
     float a, b, c=1;
-    while(1){
+    while(1){ //allows for multiple uses of the program
     printf("Enter 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division or 0 to quit: ");
     scanf("%d", &operation);
-    if(operation < 0.0 || operation > 4){
+    if(operation < 0.0 || operation > 4){//error handling for invalid input
         printf("Invalid input");
     }
-    if(operation == 0){
+    if(operation == 0){//used to quit program
         printf("Goodbye");
         return 0;
     }
@@ -47,7 +47,7 @@ int main(){
     return 0;
 }
 
-float add(float a, float b){
+float add(float a, float b){//start of the 4 different functions that handle the math
     float c;
     c = a + b;
     return c;
