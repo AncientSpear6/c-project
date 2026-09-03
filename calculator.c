be3336929896cd5a3a53a251023b7e7805290e76
Lukas <lukas.cothran@siu.edu>
2026-09-03 10:07:58 -0500

    Implemented division feature

--- calculator.c
+++ calculator.c
@@ -3,11 +3,13 @@
 int add(int a, int b);
 float sub(float a, float b);
 float mult(float a, float b);
+float div(float a, float b);
 
 int main(){
     add(5,10);
     sub(10,5);
     mult(13, 7);
+    div(3, 2);
     return 0;
 }
 
@@ -21,4 +23,15 @@ float sub(float a, float b){
 
 float mult(float a, float b){
     return a * b;
+}
+
+float div(float a, float b){
+    if(a != 0){
+        if(b != 0){
+            return a / b;
+        }
+        else{
+            printf("Cannot divide by zero");
+        }
+    }
 }
